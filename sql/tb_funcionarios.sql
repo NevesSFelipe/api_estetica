98008bf3fea9b89455fbd0d538878617726f88e5
Felipe S. Neves <felipe.s.neves@outlook.com>
2025-03-04 23:36:54 -0300

    Criando db_estetica e tb_funcionarios

---
+++ sql/tb_funcionarios.sql
@@ -0,0 +1,11 @@
+USE estetica;
+
+CREATE TABLE funcionarios (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    nome VARCHAR(50) NOT NULL,
+    email VARCHAR(100) UNIQUE NOT NULL,
+    senha VARCHAR(32) NOT NULL,
+    cargo VARCHAR(50) NOT NULL,
+    permissoes JSON,
+    ativo BOOLEAN DEFAULT TRUE
+);
