58a7d241b50680aa27d2d2975906a80801ef606f
Felipe S. Neves <felipe.s.neves@outlook.com>
2025-03-09 23:05:32 -0300

    Melhorando tratativas na API

--- core.php
+++ core.php
@@ -2,13 +2,32 @@
 
 $dados = recuperDadosRequest();
 
+validarAcaoAjax($dados['acaoAjax']);
+validarSistema($dados['sistema']);
+
 require_once 'src/controller/FuncionariosController.php';
 $funcionariosController = new FuncionariosController($dados['sistema']);
 
 header('Content-Type: application/json; charset=utf-8');
 
-if( $dados['acaoAjax'] === "funcionarioAcessarSistema" ) return $funcionariosController->acessarSistema($dados['email'], $dados['senha']);
-if( $dados['acaoAjax'] === "funcionarioSairSistema" ) return $funcionariosController->sairSistema();
+switch( $dados['acaoAjax'] ) {
+
+    case 'funcionarioAcessarSistema':
+        return $funcionariosController->acessarSistema($dados['email'], $dados['senha']);
+    break;
+
+    case 'funcionarioSairSistema':
+        return $funcionariosController->sairSistema();
+    break;
+
+    default:
+        
+        print_r(json_encode(array('status' => false, 'msg' => 'Por favor, informe uma ação válida para executar na API.'), JSON_UNESCAPED_UNICODE ));
+
+    break;
+        
+
+}
 
 function recuperDadosRequest() {
 
@@ -31,4 +50,35 @@ function recuperDadosRequest() {
 
     return $_REQUEST;
 
+}
+
+function validarAcaoAjax($acaoAjax) {
+
+    if( !isset($acaoAjax) ) {
+
+        print_r(
+            json_encode(
+                array('status' => false, 'msg' => 'Por favor, informe uma ação para executar na API.'), JSON_UNESCAPED_UNICODE 
+            )
+        );
+    
+        die();
+    
+    }
+
+}
+
+function validarSistema($sistema) {
+
+    if( !isset($sistema) || empty($sistema) ) {
+
+        print_r(
+            json_encode(
+                array('status' => false, 'msg' => 'Por favor, informe um sistema válido.'), JSON_UNESCAPED_UNICODE 
+            )
+        );
+    
+        die();
+    
+    }
 }
